6768606b86e67da9bb026f244d902f1bd56a4a0d
Gavin Draper <g@vindraper.co.uk>
2014-01-06 18:56:31 +0000

    Changed the way the feed URLs are detected to use application/rss link detection in the HTML

--- gdRead.FeedUtils/FeedUrlFinder.cs
+++ gdRead.FeedUtils/FeedUrlFinder.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using HtmlAgilityPack;
 
 namespace gdRead.FeedUtils
 {
@@ -6,13 +9,42 @@ namespace gdRead.FeedUtils
     {
         public static string FindFeedUrl(string url)
         {
-            if (url.EndsWith("feed/", StringComparison.CurrentCultureIgnoreCase) ||
-                url.EndsWith("feed", StringComparison.CurrentCultureIgnoreCase))
+            if (!url.StartsWith("http", StringComparison.InvariantCultureIgnoreCase))
+                url = "http://" + url;
+
+            //Check if URL is already a valid feed
+            try
+            {
+                using (var feedXml = XmlReader.Create(url,new XmlReaderSettings { DtdProcessing = DtdProcessing.Parse }))
+                {
+                    SyndicationFeed.Load(feedXml);
+                    feedXml.Close();
+                }
+                //No error so the url must be a valid feed
                 return url;
-            if (!url.EndsWith("/"))
-                url += "/";
-            url += "feed/";
-            return url;
+            }
+            catch
+            {
+                try
+                {
+                    var htmlLoader = new HtmlWeb();
+                    var htmlDoc = htmlLoader.Load(url);
+                    var rssLinks = htmlDoc.DocumentNode.SelectNodes("/html/head/link[@type='application/rss+xml']/@href");
+                    if(rssLinks==null)
+                        rssLinks = htmlDoc.DocumentNode.SelectNodes("/head/link[@type='application/rss+xml']/@href");
+                    //For now we take the first feed
+                    //TODO work out if multiple feeds which one to use. IE work out which is the real feed and which is comments
+                    if (rssLinks != null || rssLinks.Count > 0)
+                    {
+                        return rssLinks[0].Attributes["href"].Value;
+                    }
+                    else return "";
+                }
+                catch
+                {
+                    return "";
+                }
+            }            
         }
     }
 }
