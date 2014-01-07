d4de8a97bf9087152dc95e24e390a6f03843bdaf
Gavin Draper <g@vindraper.co.uk>
2014-01-07 08:24:25 +0000

    Read/Unread status work in progress

---
+++ gdRead.Data/Models/SubscriptionReadPost.cs
@@ -0,0 +1,9 @@
+﻿namespace gdRead.Data.Models
+{
+    public class SubscriptionReadPost
+    {
+        public int Id { get; set; }
+        public virtual Subscription Subscription { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
---
+++ gdRead.Web/Controllers/PostReadController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using gdRead.Data;
+using gdRead.Data.Models;
+using Microsoft.AspNet.Identity;
+
+namespace gdRead.Web.Controllers
+{
+	public class PostReadController : ApiController
+	{
+		[Authorize]
+		public void Post([FromBody] Post post)
+		{
+			var ctx = new gdReadContext();
+			var userId = Guid.Parse(HttpContext.Current.User.Identity.GetUserId());
+			var dbPost = ctx.Posts.FirstOrDefault(x => x.Id == post.Id);
+			var sub = ctx.Subscriptions.FirstOrDefault(x => x.Feed.Id == post.Feed.Id && x.UserId == userId);
+			var subRead = new SubscriptionReadPost()
+			{
+				Post = dbPost,
+				Subscription = sub
+			};
+			ctx.SubscriptionsSubscriptionReadPost.Add(subRead);
+			ctx.SaveChanges();
+		}
+	}
+}
