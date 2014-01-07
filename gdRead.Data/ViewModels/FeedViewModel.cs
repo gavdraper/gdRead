d081154a825b90149833e7ef6348ba8015aa70c4
Gavin Draper <g@vindraper.co.uk>
2014-01-07 21:06:04 +0000

    Changed FeedDto to FeedViewModel and moved to data project

---
+++ gdRead.Data/ViewModels/FeedViewModel.cs
@@ -0,0 +1,10 @@
+﻿using gdRead.Data.Models;
+
+namespace gdRead.Data.ViewModels
+{
+    public class FeedViewModel : Feed
+    {
+        public int UnreadCount { get; set; }
+    }
+
+}
--- gdRead.Web/Models/DTOs/FeedDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using gdRead.Data.Models;
-
-namespace gdRead.Web.Models.DTOs
-{
-    public class FeedDto : Feed
-    {
-        public bool UnreadItems { get; set; }
-    }
-}
