b6d5d33ea51d76ce7ee85c5a0b3247b8bf620cb1
Gavin Draper <g@vindraper.co.uk>
2014-01-30 08:20:12 +0000

    Tidied Routes For Friendlier URLs

--- gdRead.Web/App_Start/RouteConfig.cs
+++ gdRead.Web/App_Start/RouteConfig.cs
@@ -10,11 +10,22 @@ namespace gdRead.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "UnAuthenticatedHomePage",
+                url: "Home/",
+                defaults: new { controller = "Home", action = "Index" }
             );
 
+            routes.MapRoute(
+                name: "Dashboard",
+                url: "Dev/",
+                defaults: new { controller = "DevTest", action = "Dashboard" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "MyFeeds", id = UrlParameter.Optional }
+            );
         }
     }
 }
